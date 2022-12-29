e4222495acd0fd9e072c768df7dbb1d8f00efecd
Alexandr <hitrin79@gmail.com>
2022-12-29 23:48:13 +0800

    Task 56

--- Task 56/Task 56/Program.cs
+++ Task 56/Task 56/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int m = ReadLine("Введите M ");
+int n = ReadLine("Введите N ");
+int[,] matrix = new int[m, n];
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+MinSumLine(matrix);
+
+
+void MinSumLine(int [,] matrix)
+{
+    int minSum = 0;
+    int indexRow = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];
+        }
+            if (i == 0)
+            {
+                minSum = sum;
+            }
+            else if (sum < minSum)
+            {
+                minSum = sum;
+                indexRow = i + 1;
+            }
+    }
+    Console.WriteLine($"Строка номер {indexRow}, содержит в себе наименьшую сумму элементов {minSum}");
+
+}
+
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int ReadLine(string input)
+{
+    Console.Write(input);
+    int output = int.Parse(Console.ReadLine()!);
+    return output;
+}
